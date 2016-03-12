a48f2d3481a3eace528eb8e885d98094b9c6452f
Jemsann <jemma.touma@gmail.com>
2016-03-12 18:48:44 +0100

    Client och Server

--- StabberServer/Program.cs
+++ StabberServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,51 +10,96 @@ namespace StabberServer
 {
     class Program
     {
-        static void Main(string[] args)
+		const String IPADDRESS = "127.0.0.1";
+		//const String IPADDRESS = "10.56.5.232";
+		const Int32 PORT = 8001;
+		static IPAddress ipAddress = IPAddress.Parse(IPADDRESS);
+		static IPEndPoint localEndPoint = new IPEndPoint(ipAddress, PORT);
+		static UTF8Encoding encoding = new UTF8Encoding();
+		private static Socket socket = null;
+		private static Socket listeningSocket = null;
+
+		const int BUFFERLENGTH = 100;
+		static string response = string.Empty;
+
+		static void Main(string[] args)
         {
+			listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			listeningSocket.Bind(localEndPoint);
 
-           
+			socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			listeningSocket.Listen(100);
+			Byte[] bufferIn = new Byte[BUFFERLENGTH];
+			socket = listeningSocket.Accept();
+			socket.Receive(bufferIn);
+			
+			string requestIn = encoding.GetString(bufferIn);
+			String[] tokens = requestIn.Split(' ');
+			if(tokens[0] != "DDO/1.0")
+			{
+				response = "DDO/1.0 ERROR Use DDO/1.0 protocol";			
+			}
+			else 
+			{
+				switch (tokens[1])
+				{
+					case "LOGIN": CheckLogIn(tokens);
+						break;
+					default: break;
+				}
+			}
+			Byte[] bufferOut = encoding.GetBytes(response);
+			socket.Send(bufferOut);
 
-        }
 
-        static Boolean ReceiveRequest(Socket socket, String opponent,
-            out Int32 row, out Int32 column)
-        {
-            row = -1; column = -1;
-            Int32 threadId = Thread.CurrentThread.ManagedThreadId;
-            Byte[] bufferIn = new Byte[BUFFERLENGTH];
-            socket.Receive(bufferIn);
-            String request = encoding.GetString(bufferIn).TrimEnd('\0').Trim();
-            Console.WriteLine(threadId + ": Received request from " + socket.RemoteEndPoint + ": " + request);
-            String[] tokens = request.Split(' ');
-            Boolean ok = tokens.Length == 5 &&
-                tokens[0] == "BAP/1.1" && tokens[1] == "SHOT" &&
-                Int32.TryParse(tokens[2], out row) &&
-                row >= 0 && row < BOARDSIDE &&
-                Int32.TryParse(tokens[3], out column) &&
-                column >= 0 && column < BOARDSIDE &&
-                tokens[4] == opponent;
-            return ok;
-        }
 
-        static void SendResponse(Socket socket, Boolean ok, Square[,] board,
-            Int32 row, Int32 column, String opponent)
-        {
-            Int32 threadId = Thread.CurrentThread.ManagedThreadId;
-            Boolean isHit = false;
-            ok = ok && row >= 0 && row < BOARDSIDE
-                && column >= 0 && column < BOARDSIDE;
-            if (ok)
-                isHit = board[row, column] == Square.SHIP;
-            String response = null;
-            if (ok)
-                response = "BAP/1.1 HIT " + row + " " + column +
-                    " " + isHit.ToString().ToUpper() + " " + opponent;
-            else
-                response = "BAP/1.1 ERROR Bad request.";
-            Byte[] bufferOut = encoding.GetBytes(response);
-            socket.Send(bufferOut);
-            Console.WriteLine(threadId + ": Sent response to " + socket.RemoteEndPoint + ": " + response);
-        }
+		}
+
+		static void CheckLogIn(string[] tokens)
+		{
+			PlayerDbContext playerDB = new PlayerDbContext();
+
+			response = $"DDO/1.0 LOGIN ACCEPTED {tokens[2]} {tokens[3]}";
+		}
+
+        //static Boolean ReceiveRequest(Socket socket, String opponent,
+        //    out Int32 row, out Int32 column)
+        //{
+        //    row = -1; column = -1;
+        //    Int32 threadId = Thread.CurrentThread.ManagedThreadId;
+        //    Byte[] bufferIn = new Byte[BUFFERLENGTH];
+        //    socket.Receive(bufferIn);
+        //    String request = encoding.GetString(bufferIn).TrimEnd('\0').Trim();
+        //    Console.WriteLine(threadId + ": Received request from " + socket.RemoteEndPoint + ": " + request);
+        //    String[] tokens = request.Split(' ');
+        //    Boolean ok = tokens.Length == 5 &&
+        //        tokens[0] == "BAP/1.1" && tokens[1] == "SHOT" &&
+        //        Int32.TryParse(tokens[2], out row) &&
+        //        row >= 0 && row < BOARDSIDE &&
+        //        Int32.TryParse(tokens[3], out column) &&
+        //        column >= 0 && column < BOARDSIDE &&
+        //        tokens[4] == opponent;
+        //    return ok;
+        //}
+
+        //static void SendResponse(Socket socket, Boolean ok, Square[,] board,
+        //    Int32 row, Int32 column, String opponent)
+        //{
+        //    Int32 threadId = Thread.CurrentThread.ManagedThreadId;
+        //    Boolean isHit = false;
+        //    ok = ok && row >= 0 && row < BOARDSIDE
+        //        && column >= 0 && column < BOARDSIDE;
+        //    if (ok)
+        //        isHit = board[row, column] == Square.SHIP;
+        //    String response = null;
+        //    if (ok)
+        //        response = "BAP/1.1 HIT " + row + " " + column +
+        //            " " + isHit.ToString().ToUpper() + " " + opponent;
+        //    else
+        //        response = "BAP/1.1 ERROR Bad request.";
+        //    Byte[] bufferOut = encoding.GetBytes(response);
+        //    socket.Send(bufferOut);
+        //    Console.WriteLine(threadId + ": Sent response to " + socket.RemoteEndPoint + ": " + response);
+        //}
     }
 }
